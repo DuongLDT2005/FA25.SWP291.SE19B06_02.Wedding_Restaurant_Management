45825ffaf9927c5fed416bf8a213f1e9d19e2113
lethuyduong <lethuyduongc9@gmail.com>
2025-09-21 16:39:13 +0700

    add login page

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import LoginPage from "../pages/auth/LoginPage";
+
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* Thêm các route khác tại đây */}
       </Routes>
     </BrowserRouter>
   );
