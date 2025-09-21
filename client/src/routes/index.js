cfa1e444e6938e6221971eadaed7538e4260b1ed
Uyen <phuyen.gauuniform@gmail.com>
2025-09-22 02:00:14 +0700

    feat: add signup customer

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import SignUp from "../pages/auth/signUpCus";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
