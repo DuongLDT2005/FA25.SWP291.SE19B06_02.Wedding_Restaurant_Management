bc6078b6ee6331a6a78cc8051952c6e5fa562c0c
DuckNeoNULL <khiemtktktktk123@gmail.com>
2025-09-28 21:47:42 +0700

    FINISH CRUD HALL

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import restaurantRoutes from "./routes/restaurantRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import hallRoutes from "./routes/hallRoutes.js";
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/restaurants",restaurantRoutes);
+app.use("/api/restaurants/halls", hallRoutes);
 app.use("/api/auth",authRoutes);
 app.use("/api/admin",userRoutes);
 export default app;
